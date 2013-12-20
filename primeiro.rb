472624b5689a9cf0b5464907dc6c9fe34bb40d63
Didox <dansantos@abril.com.br>
2013-12-20 10:13:19 -0200

    atualizando apostila

--- primeiro.rb
+++ primeiro.rb
@@ -1,6 +1,214 @@
 # -*- encoding : utf-8 -*-
 
+require 'debugger'
+
+module Funcoes
+  def self.included(o)
+    o.extend(Staticos)
+  end
+
+  module Staticos
+    def calcula_cep
+      "calculado cep"
+    end
+  end
+
+
+  def calcula_cpf
+    "calculado"
+  end
+
+end
+
+
+module Aula
+  class Klass
+
+    include Funcoes
+
+    CONSTANTE = "abril"
+
+    def metodo_instancia
+      "criar novo instancia #{CONSTANTE}"
+    end
+
+    def self.metodo_de_classe_ou_statico
+      "criar novo instancia #{CONSTANTE}"
+    end
+
+    def method_missing(meth) 
+      return attrs[meth.to_sym] if attrs.has_key?(meth.to_sym)
+      raise "elemento não encontrado"
+    end
+
+    private
+
+    def attrs 
+      {:key1 => "key1 value", :key2 => "key2 value"}
+    end
+
+  end
+end
+
+puts Aula::Klass.new.key1
+
+debugger
+
+x = ""
+
+
+
+
+
+=begin
+
+
+
+
+
+
+
+
+
+
+
+
+class Singleton
+  private_class_method :new
+
+  def self.statico(param = nil)
+    "criar novo instancia #{param}"
+  end
+
+  def self.statico_vanessa(param = "vanessa")
+    "criar novo instancia #{param}"
+  end
+end
+
+
+
+class Carro
+  attr_accessor :nome, :ano, :quantidade_de_portas, :cor
+
+  @volante = "de couro"
+
+  def marca
+    @volante = "xyx"
+    @volante
+  end
+
+  private
+
+  def metodo_privado
+    "privado"
+  end
+
+
+  protected
+
+  def metodo_protegido
+    "protegido"
+  end
+end
+
+
+class Fiat < Carro
+  attr_accessor :modelo
+
+  def marca
+    "FIAT - #{super}"
+  end
+
+  def venderdor
+    "Itavema"
+  end
+
+  def set_marca(valor)
+    @set_marca = "#{self.marca} - #{valor}"
+  end
+
+  def get_marca
+    @set_marca
+  end
+
+  private
+
+  def calcula_marca
+     "ssss"
+  end
+
+  public
+
+  def calcula_marca2
+    "ssss #{self.metodo_privado}"
+  end
+
+end
+
+
+
+c = Fiat.new
+puts c.calcula_marca2
+
+
+
+
+
+class VW < Carro
+  attr_accessor :modelo
+
+  def marca
+    "Volks"
+  end
+
+  def vendedor
+    "Comeri"
+  end
+end
+
+
+# c = Fiat.new
+# c.set_marca("didox")
+# puts c.get_marca
+
+
+# c = Carro.new
+# c.nome = "teste"
+# c.ano = "2005"
+# c.quantidade_de_portas = 4
+# c.cor = "branco"
+# puts c.inspect
+
+# c = VW.new
+# c.nome = "teste"
+# c.ano = "2005"
+# c.quantidade_de_portas = 4
+# c.cor = "branco"
+# c.modelo = "modelo vw"
+# puts c.inspect
+
+# c = Fiat.new
+# c.nome = "teste"
+# c.ano = "2005"
+# c.quantidade_de_portas = 4
+# c.cor = "branco"
+
+# puts c.inspect
+
+
+
+=begin
+
+
+
+
+
+
+
 class Modo
+  @@nome = "ss"
+
+
   def p1
     @p1
   end
@@ -20,6 +228,22 @@ class Modo
   def nome_preenchido?
     @nome.present?
   end
+
+end
+
+
+
+
+
+
+def numero(index)
+  return "numero 1" if index == 1
+  return "numero 2" if index == 2
+  return "numero 3" if index == 3
+  return "numero 5" if index == 4
+
+  "defaulr"
+
 end
 
 
@@ -55,9 +279,62 @@ if 1 == 1
   puts "didox"
 end
 
-=end
 
 puts "didox"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"aula de ruby 2"
+
+
+modo.faz_replace("aula de ruby 2", "aula", "aula ruby")
+
+
+"aula ruby de ruby 2"
+
+
+modo.tranforma_em_constante("abril")
+
+"Abril"
+
+
+modo.maiuscula("abril")
+
+"ABRIL"
+
+
+modo.minuscula("ABRIL")
+
+"abril"
+
+
+modo.esta_em_maiuscula?("ABRIL")
+
+true
+
+
+=end
+
+
+
+
+
 
 
 
